7427cdac800530af23ce3ead4efc503fb89242f2
nobrokenboy <2335966301@qq.com>
2016-12-07 15:25:14 +0800

    新增工具方法以及ideal-image插件

--- util.js
+++ util.js
@@ -3,6 +3,8 @@
  */
 window.util=(function(){
     var util={};
+    util.url=window.location.href;
+    util.search=window.location.search;
     util.hasClass=function(obj,classOfName){
         return obj.className.match(new RegExp('(\\s|^)'+classOfName+'(\\s|$)'));
     };
@@ -24,6 +26,89 @@ window.util=(function(){
         }else{
             this.addClass(obj,classOfName);
         }
+    };
+    /*
+    * change to Array,the method is better than the next one
+    * */
+    util.toArr= function (items) {
+            try {
+                return Array.prototype.slice.call(items);
+            } catch (ex) {
+
+                var i= 0,
+                    len= items.length,
+                    result= Array(len);
+
+                while (i < len) {
+                    result[i] = items[i];
+                    i++;
+                }
+
+                return result;
+            }
+    };
+    util.toArray= function (obj) {
+        return Array.prototype.slice.call(obj);
+    };
+    /*
+    * 检测对象的类型
+    * */
+    util.isType = function(type, obj) {
+        //检测数组可以使用Object.prototype.toString()方法进行检测，如果是数组的话，他会返回"[object Array]",如果是对象，会返回"[object object]"
+        var _class = Object.prototype.toString.call(obj).slice(8, -1);
+        return obj !== undefined && obj !== null && _class === type;
+    };
+    /*
+    *深度扩展
+    * */
+    util.deepExtend = function(out) {
+        out = out || {};
+        for (var i = 1; i < arguments.length; i++) {
+            var obj = arguments[i];
+            if (!obj)
+                continue;
+            for (var key in obj) {
+                //检测该对象属性是不是从父类那边继承来的
+                if (obj.hasOwnProperty(key)) {
+                    if (this.isType('Object', obj[key]) && obj[key] !== null)
+                        this.deepExtend(out[key], obj[key]);
+                    else
+                        out[key] = obj[key];
+                }
+            }
+        }
+        return out;
+    };
+    /*
+    *去重数组的函数
+    * */
+    util.removeDuplicate=function(arr){
+        var obj={},
+            result=[];
+        for(var i= 0,j=arr.length;i<j;i++){
+            obj[arr[i]]=i;
+        }
+        //获取对象的key
+        for(var i in obj){
+            result.push(i);
+        }
+        return result;
+    }
+    /*
+    *传入地址栏参数获取对应的值；
+    */
+    util.getUrlParas=function(para){
+        var paraStr,arr=[];
+        paraStr=this.search.substr(1);
+        arr=paraStr.split("&");
+        for(var i=0,j=arr.length;i<j;i++){
+            var parameter=arr[i].split("=");
+            if(parameter[0].toLowerCase()==para.toLowerCase()){
+                return parameter[1];
+            }
+        }
+        return "";
+
     };
     return util;
 })();
