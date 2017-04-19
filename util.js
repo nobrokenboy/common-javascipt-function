74e6fbddb3a4d31d44fdecba6a9a93bd9dbb5b19
nobrokenboy <2335966301@qq.com>
2017-04-19 17:28:51 +0800

    增加一些公用方法 :smile:

--- util.js
+++ util.js
@@ -93,7 +93,15 @@ window.util=(function(){
             result.push(parseInt(i));
         }
         return result;
-    }
+    };
+    /*
+    *判断地址了是否存在该字段
+     * */
+    util.isIndexOfValue=function(val){
+		if(this.url.indexOf(val)>-1){
+			return true;
+		}
+	};
     /*
     *传入地址栏参数获取对应的值；
     */
@@ -104,8 +112,7 @@ window.util=(function(){
         for(var i=0,j=arr.length;i<j;i++){
             var parameter=arr[i].split("=");
             if(parameter[0].toLowerCase()==para.toLowerCase()){
-            	//避免获取的参数中含中文名称乱码的问题
-                return decodeURI(parameter[1]);
+                return parameter[1];
             }
         }
         return "";
@@ -185,6 +192,34 @@ window.util=(function(){
 		var m=dd.getMonth()+1;
 		var d=dd.getDate();
 		return y+"-"+m+"-"+d;
+	};
+	
+	//获取统一为“-”连接符的日期格式
+	util.getDateToday=function(){
+		var tempDate=new Date();
+		var nowYear=tempDate.getFullYear();
+		var nowMonth=tempDate.getMonth()+1;
+		var nowDate=tempDate.getDate();
+		var nowHour=tempDate.getHours()>9?tempDate.getHours():"0"+tempDate.getHours();
+		var nowMini=tempDate.getMinutes()>9?tempDate.getMinutes():"0"+tempDate.getMinutes();
+		var nowSecond=tempDate.getSeconds()>9?tempDate.getSeconds():"0"+tempDate.getSeconds();
+		return nowYear+"-"+nowMonth+"-"+nowDate+" "+nowHour+":"+nowMini+":"+nowSecond;
+	};
+	
+	//判断身份证是否正确
+	util.fisCardID=function (sId) {
+		var aCity={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外"}
+		var iSum=0 ;
+		var info="" ;
+		if(!/^\d{17}(\d|x)$/i.test(sId)) return "你输入的身份证长度或格式错误";
+		sId=sId.replace(/x$/i,"a");
+		if(aCity[parseInt(sId.substr(0,2))]==null) return "你的身份证地区非法";
+		sBirthday=sId.substr(6,4)+"-"+Number(sId.substr(10,2))+"-"+Number(sId.substr(12,2));
+		var d=new Date(sBirthday.replace(/-/g,"/")) ;
+		if(sBirthday!=(d.getFullYear()+"-"+ (d.getMonth()+1) + "-" + d.getDate()))return "身份证上的出生日期非法";
+		for(var i = 17;i>=0;i --) iSum += (Math.pow(2,i) % 11) * parseInt(sId.charAt(17 - i),11) ;
+		if(iSum%11!=1) return "你输入的身份证号非法";
+		return true;
 	};
     
     
