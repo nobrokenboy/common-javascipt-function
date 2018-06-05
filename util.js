a13d1d35cc6bcad056154ffc91872d1e2178669a
chenshuyang <chenshuyang@o-home.com>
2018-06-05 11:15:05 +0800

    提交下去json数组重复的函数

--- util.js
+++ util.js
@@ -396,6 +396,20 @@ window.util=(function(){
 		}
 		
 	}
+	util.fisCardID=function (sId) {
+		var aCity={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外"}
+		var iSum=0 ;
+		var info="" ;
+		if(!/^\d{17}(\d|x)$/i.test(sId)) return "你输入的身份证长度或格式错误";
+		sId=sId.replace(/x$/i,"a");
+		if(aCity[parseInt(sId.substr(0,2))]==null) return "你的身份证地区非法";
+		sBirthday=sId.substr(6,4)+"-"+Number(sId.substr(10,2))+"-"+Number(sId.substr(12,2));
+		var d=new Date(sBirthday.replace(/-/g,"/")) ;
+		if(sBirthday!=(d.getFullYear()+"-"+ (d.getMonth()+1) + "-" + d.getDate()))return "身份证上的出生日期非法";
+		for(var i = 17;i>=0;i --) iSum += (Math.pow(2,i) % 11) * parseInt(sId.charAt(17 - i),11) ;
+		if(iSum%11!=1) return "你输入的身份证号非法的哦";
+		return true;
+	};
 	
 	//判断是不是正整数
 	util.testIsPositiveInt=function(val){
@@ -455,5 +469,38 @@ window.util=(function(){
 			return false;
 		}
 	};
+	util.getDateStr=function(addDayNums){//获取今，明，后等的日期
+        const date=new Date();
+        date.setDate(date.getDate()+addDayNums);
+        let year=date.getFullYear();
+        let month=date.getMonth()+1;
+        let day=date.getDate();
+        return `${year}-${month}-${day}`;
+    }
+	
+	util.dealWidthRepeateObj=function(arr,differ){//arr是去重的数组对象,differ是key,3个以上数据存在问题
+//	
+//		 for (var i = 0; i < arr.length - 1; i++) {//出现3个以上就去重不了
+//            for (var j = 1; j < arr.length; j++) {
+//                if (i != j) {
+//                    if (arr[i][differ] == arr[j][differ]) {
+//                        arr.splice(j, 1);
+//                    }
+//                }
+//
+//            }
+//        }
+
+			for(var i = 0; i < arr.length - 1; i++) {  
+		        for(var j = i+1; j < arr.length; j++) {  
+		          if(arr[i][differ] ==arr[j][differ]) {  
+		            arr.splice(j, 1)  
+		            j = j-1  
+		          }  
+		        }  
+		      }  
+		 
+		 return arr;
+	}
     return util;
 })();
